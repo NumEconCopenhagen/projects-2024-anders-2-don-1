0bf4ca4bcd835c49b6af650b188578d7572f74f8
mkt680 <mkt680@alumni.ku.dk>
2024-03-13 15:33:08 +0100

    Opgave 1 er klaret

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,52 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
+from types import SimpleNamespace
+
+
+class ExchangeEconomyClass:
+
+    def __init__(self):
+
+        par = self.par = SimpleNamespace()
+
+        # a. preferences
+        par.alpha = 1/3
+        par.beta = 2/3
+
+        # b. endowments
+        par.w1A = 0.8
+        par.w2A = 0.3
+
+    def utility_A(self,x1A,x2A):
+        """Calculate utility for consumer A given quantities of goods x1A and x2A."""
+        return (x1A ** self.par.alpha) * (x2A ** (1 - self.par.alpha))
+
+    def utility_B(self,x1B,x2B):
+        """Calculate utility for consumer B given quantities of goods x1B and x2B."""
+        return (x1B ** self.par.beta) * (x2B ** (1 - self.par.beta))
+
+
+    def demand_A(self,p1):
+        budget = self.par.w1A * p1 + self.par.w2A
+        x1A = (self.par.alpha / p1) * budget
+        x2A = ((1 - self.par.alpha) / 1) * budget  # p2 is the numeraire and equals 1
+        return x1A, x2A
+
+
+    def demand_B(self,p1):
+        w1B = 1 - self.par.w1A
+        w2B = 1 - self.par.w2A
+
+        budget = self.par.w1B * p1 + self.par.w2B
+        x1B = (self.par.beta / p1) * budget
+        x2B = ((1 - self.par.beta) / 1) * budget  # p2 is the numeraire and equals 1
+        return x1B, x2B
+
+    def check_market_clearing(self, p1):
+        """Check whether the market clears for a given price p1."""
+        x1A, x2A = self.demand_A(p1)
+        x1B, x2B = self.demand_B(p1)
+
+        # Calculate excess demand for both goods
+        eps1 = x1A + x1B - 1  # Total endowment for good 1 is 1
+        eps2 = x2A + x2B - 1  # Total endowment for good 2 is 1
+
+        return eps1, eps2
